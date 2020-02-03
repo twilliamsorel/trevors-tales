464fe8a2cdea7119dd9e26d8c4421eef3b125272
Trevor Sorel <trevorsorel@Trevors-Mini-37.hsd1.ca.comcast.net>
2020-02-02 17:57:53 -0800

    making popup only fire on story pages; adding toggle feature

--- src/_assets/js/sub-popup.js
+++ src/_assets/js/sub-popup.js
@@ -1,18 +1,30 @@
 (function () {
   var popup = document.querySelector('.subscription-modal');
-  var close = popup.querySelector('[data-close-popup]');
-  
-  close.addEventListener('click', function () {
-    popup.classList.add('removed');
-    localStorage.hideSubscribe = true;
-  });
 
-  if (!localStorage.hideSubscribe) {
-    setTimeout(function () {
-      popup.classList.remove('removed');
+  if (popup) {
+    var close = popup.querySelector('[data-close-popup]');
+    var cta = document.querySelector('[data-cta-marker]');
+    
+    close.addEventListener('click', function () {
+      popup.classList.add('removed');
+      localStorage.hideSubscribe = true;
+    });
+
+    if (!localStorage.hideSubscribe) {
       setTimeout(function () {
-        popup.style.opacity = '1';
-      }, 100);
-    }, 90000);
+        popup.classList.remove('removed');
+        setTimeout(function () {
+          popup.style.opacity = '1';
+        }, 100);
+      }, 90000);
+    }
+
+    window.addEventListener('scroll', function () {
+      if ((cta.getBoundingClientRect().top - window.innerHeight) < 0 && !localStorage.hideSubscribe) {
+        popup.classList.add('removed');
+      } else if (!localStorage.hideSubscribe) {
+        popup.classList.remove('removed');
+      }
+    });
   }
 }());
