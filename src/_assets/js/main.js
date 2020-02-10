3c40c5555173c543ac92061ddc12fccdcd0c2c87
Trevor Sorel <trevorsorel@Trevors-Mini-12.hsd1.ca.comcast.net>
2020-02-09 16:03:16 -0800

    removing popup

--- src/_assets/js/main.js
+++ src/_assets/js/main.js
@@ -5,7 +5,6 @@
 //= require ./forms.js
 //= require ./suggestion-tracker.js
 //= require ./cat-nav.js
-//= require ./sub-popup.js
 
 if (window.netlifyIdentity) {
   window.netlifyIdentity.on("init", function (user) {
