b1af7345b53ff84ddb5091f84d88a5d9df5a604b
Trevor Sorel <twilliamsorel@gmail.com>
2023-02-02 11:02:51 -0700

    adding data plugin

--- src/_assets/js/main.js
+++ src/_assets/js/main.js
@@ -5,13 +5,3 @@
 //= require ./forms.js
 //= require ./suggestion-tracker.js
 //= require ./cat-nav.js
-
-if (window.netlifyIdentity) {
-  window.netlifyIdentity.on("init", function (user) {
-    if (!user) {
-      window.netlifyIdentity.on("login",  function () {
-        document.location.href = "/admin/";
-      });
-    } 
-  });
-}
